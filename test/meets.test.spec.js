195a130173d1a15005bba24af385b4f7b8429691
Ken Robinson <subscriptions@kodehaus.com>
2021-03-24 12:40:13 -0400

    updated 200 & 400 status tests

--- test/meets.test.spec.js
+++ test/meets.test.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const path = require('path');
 const fs = require('fs');
 const fsPromise = require('fs').promises;
-const axios = require('axios');
+const axios = require('axios').default;
 
 
 // Helper method to identify if files exist within the file system
@@ -25,4 +25,45 @@ describe('Mocha', function () {
   it('should execute properly', function () {
     expect(true).to.be.true;
   });
+});
+
+describe('meets expectations', function () {
+  describe('Validate instructions 01', function () {
+    describe('.gitignore', function () {
+      it('file should exist', async function () {
+        const actual = await testItemExists(global.appRoot + '.gitignore');
+        expect(actual).to.be.true;
+      });
+      it('file should contain node_modules', async function () {
+        const actual = await testFileContainsString(global.appRoot + '.gitignore', 'node_modules');
+        expect(actual).to.be.true;
+      });
+      // end of git ignore check
+    });
+
+    describe('application', function () {
+
+      it('should return a 200 status when accessing http://localhost:5000', async function () {
+        const actual = await (await axios.get('http://localhost:5000')).status;
+        const expected = 200;
+        expect(actual).to.equal(expected);
+      });
+
+      it('should return a 404 status when accessing unknown page http://localhost:5000/12qw@$@$@iijjhg3cfdhf', async function () {
+        let actual = null;
+        let expected = 404;
+        try{
+          actual = await (await axios.get('http://localhost:5000/12qw@$@$@iijjhg3cfdhf')).status;
+        } catch(err){
+          actual = err.response.status;
+        }
+        expect(actual).to.equal(expected);
+      });
+
+    });
+
+
+    // end of validate instructions 01
+  });
+  // end of meets 
 });
